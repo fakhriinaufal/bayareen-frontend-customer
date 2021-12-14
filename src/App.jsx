9047a5683083c1cf0c73b081004e74c76b82e8f1
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2021-12-14 17:57:54 +0800

    create vite project with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="font-bold text-2xl text-blue-900">
+          React and Tailwind with Vitejs!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
