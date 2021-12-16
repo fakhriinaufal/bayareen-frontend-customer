17cffa8ec29161d6f5a0d7aac2c84ead45a5d6d1
Francisco <fcisco.dev@gmail.com>
2021-12-16 10:30:59 +0700

    move colors object to extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    colors: {
-      'dark-green': {
-        DEFAULT: '#006D77',
-        'hover': '#085860'
+    extend: {
+      colors: {
+        'dark-green': {
+          DEFAULT: '#006D77',
+          'hover': '#085860'
+        },
+        'light-gray': {
+          DEFAULT: '#EDF6F9'
+        },
       },
-      'light-gray': {
-        DEFAULT: '#EDF6F9'
-      },
-      white: colors.white,
     },
   },
   plugins: [],
