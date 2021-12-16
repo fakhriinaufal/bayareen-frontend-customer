cc415b1682407ad9f53362a27d4de3a5716aa1e4
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2021-12-16 16:58:02 +0800

    create header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-green": {
+          DEFAULT: "#006D77",
+          hover: "#085860",
+        },
+        "light-green": {
+          DEFAULT: "#83C5BE",
+        },
+        "light-gray": {
+          DEFAULT: "#EDF6F9",
+        },
+      },
+      fontFamily: {
+        title: ["Rancho"],
+      },
+    },
   },
   plugins: [],
 };
