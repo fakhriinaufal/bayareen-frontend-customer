bc711493630ab37e4b0623d3bfc6e7f8209abda3
Francisco <fcisco.dev@gmail.com>
2021-12-16 09:22:07 +0700

    extend some color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'dark-green': {
+        DEFAULT: '#006D77',
+        'hover': '#085860'
+      },
+      'light-gray': {
+        DEFAULT: '#EDF6F9'
+      },
+      white: colors.white,
+    },
   },
   plugins: [],
 };
