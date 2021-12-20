81e2e927d61e89cfaf71313e90e04e468d21d9d5
Francisco <fcisco.dev@gmail.com>
2021-12-20 16:48:09 +0700

    change navbar size and add boxShadow

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,23 +6,23 @@ import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <div className='bg-light-gray bottom-0 min-w-full h-24 absolute text-dark-green font-semibold flex justify-around items-center py-12'>
+        <div className='bg-light-gray bottom-0 min-w-full h-16 absolute text-dark-green font-semibold flex justify-around items-center pt-2 shadow-navbar'>
             <NavLink to='' className={(param) => param.isActive ? 'font-bold border-b-2 border-dark-green' : ''}>
                 <div className='flex flex-col justify-center items-center'>
-                    <img className='w-7 mb-1' src={home} alt="home-icon" />
-                    <p>Home</p>
+                    <img className='w-5 mb-1' src={home} alt="home-icon" />
+                    <p className='text-sm'>Home</p>
                 </div>
             </NavLink>
             <NavLink to='/transaction' className={(param) => param.isActive ? 'font-bold border-b-2 border-dark-green' : ''}>
                 <div className='flex flex-col justify-center items-center'>
-                    <img className='w-7 mb-1' src={transaction} alt="transaction-icon" />
-                    <p>Transaction</p>
+                    <img className='w-5 mb-1' src={transaction} alt="transaction-icon" />
+                    <p className='text-sm'>Transaction</p>
                 </div>
             </NavLink>
             <NavLink to='/profile' className={(param) => param.isActive ? 'font-bold border-b-2 border-dark-green' : ''}>
                 <div className='flex flex-col justify-center items-center'>
-                    <img className='w-7 mb-1' src={profile} alt="profile-icon" />
-                    <p>Profile</p>
+                    <img className='w-5 mb-1' src={profile} alt="profile-icon" />
+                    <p className='text-sm'>Profile</p>
                 </div>
             </NavLink>
         </div>
