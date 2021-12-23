dc5556d8494ba29686cb71212e111474d8862e16
Francisco <fcisco.dev@gmail.com>
2021-12-23 18:36:11 +0700

    add dropdown components and initial state

--- src/pages/CheckoutForm/CheckoutAir.jsx
+++ src/pages/CheckoutForm/CheckoutAir.jsx
@@ -3,8 +3,24 @@ import HeaderSecond from "../../components/Header/HeaderSecond";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import Dropdown from "../../components/Dropdown/Dropdown"
+import { useState } from "react";
 
 export default function CheckoutAir() {
+  const [provider, setProvider] = useState({
+    val: null,
+    text: "Select",
+  })
+  
+  // const mock = [
+  //   {
+  //       text: "Option 1",
+  //       val: 1,
+  //   },
+  //   {
+  //       text: "Option 2",
+  //       val: 2,
+  //   }
+  // ]
   return (
     <Layout head={<HeaderSecond />}>
       <div className="flex flex-col">
@@ -13,7 +29,7 @@ export default function CheckoutAir() {
         </div>
         <form action="" className="">
           <Input text={"Nomor Pelanggan"} type={"number"} />
-          {/* ini tempat dropdown */}
+          <Dropdown text={"Wilayah"} name={"wilayah"} list={mock} value={provider} containerClassName={"mt-5"} onChange={setProvider} />
           <Button text={"Checkout"} className={"mt-10"} />
         </form>
       </div>
--- src/pages/CheckoutForm/CheckoutPaket.jsx
+++ src/pages/CheckoutForm/CheckoutPaket.jsx
@@ -2,8 +2,29 @@ import Layout from "../../components/Layout/Layout";
 import HeaderSecond from "../../components/Header/HeaderSecond";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import Dropdown from "../../components/Dropdown/Dropdown"
+import { useState } from "react";
 
 export default function CheckoutPaket() {
+  const [provider, setProvider] = useState({
+    val: null,
+    text: "cari provider",
+  })
+  const [nominal, setNominal] = useState({
+    val: null,
+    text: "100xxxx",
+  })
+
+  // const mock = [
+  //   {
+  //       text: "Option 1",
+  //       val: 1,
+  //   },
+  //   {
+  //       text: "Option 2",
+  //       val: 2,
+  //   }
+  // ]
   return (
     <Layout head={<HeaderSecond />}>
       <div className="flex flex-col">
@@ -12,8 +33,8 @@ export default function CheckoutPaket() {
         </div>
         <form action="" className="">
           <Input text={"Nomor"} type={"number"} />
-          {/* ini tempat dropdown */}
-          {/* ini tempat dropdown */}
+          <Dropdown text={"Provider Pulsa"} name={"provider"} list={mock} value={provider} containerClassName={"mt-5"} onChange={setProvider} />
+          <Dropdown text={"Nominal"} name={"nominal"} list={mock} value={nominal} containerClassName={"mt-5"} onChange={setNominal} />
           <Button text={"Checkout"} className={"mt-10"} />
         </form>
       </div>
