a5d1ae1d5d5dbfacd2afea62617a4d8622f430be
Francisco <fcisco.dev@gmail.com>
2021-12-24 16:54:10 +0700

    add swiper component to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,61 @@
 import Layout from "../../components/Layout/Layout";
 import Header from "../../components/Header/HeaderMain";
 import Navbar from "../../components/Navbar/Navbar";
+import Category from "./Category";
 import Banner from "../../assets/img/MainBanner.png";
 import simpati from "../../assets/img/simpati.png";
 import xl from "../../assets/img/xl.png";
-
-import Category from "./Category";
-
+// Import Swiper components
 import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+// import Swiper core and required modules
+import SwiperCore, { FreeMode } from "swiper";
 
+SwiperCore.use([FreeMode]);
 export default function Home(props) {
+  const mock = [
+    {
+      name: "Pulsa 50.000",
+      src: simpati,
+      alt: "simpati",
+    },
+    {
+      name: "Paket 5GB",
+      src: xl,
+      alt: "xl",
+    },
+  ];
+
   return (
     <Layout head={<Header />} nav={<Navbar />}>
       <div className="flex flex-col">
         <img src={Banner} alt="banner" className="mt-16 rounded-lg h-36" />
         <div className="mt-6 mb-2 text-dark-green font-bold">Categories</div>
-        <div className="bg-light-gray flex flex-wrap rounded-lg items-center">
+        <div className="bg-light-gray flex flex-wrap rounded-lg items-center p-2">
           {props.data.map((item) => (
             <Category key={item.id} data={item} />
           ))}
         </div>
         <div className="mt-6 mb-2 text-dark-green font-bold">New Products</div>
 
-        <Swiper spaceBetween={50} slidesPerView={3}>
-          <SwiperSlide>
-            <div className="bg-light-gray flex rounded-lg items-center px-3 py-2 w-56">
-              <div className="inline-flex items-center">
-                <img src={simpati} alt="simpati" />
-                <div>Pulsa 50.000</div>
+        <Swiper
+          freeMode={true}
+          className="mySwiper w-full"
+          slidesPerView={"auto"}
+          spaceBetween={30}
+          pagination={{ clickable: true }}
+        >
+          {mock.map((data) => (
+            <SwiperSlide style={{ width: "14rem" }}>
+              <div className="bg-light-gray flex rounded-lg items-center px-3 py-2 w-56">
+                <div className="inline-flex gap-2 items-center">
+                  <img src={data.src} alt={data.alt} />
+                  <div>{data.name}</div>
+                </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="bg-light-gray flex rounded-lg items-center px-3 py-2 w-56">
-              <div className="inline-flex items-center">
-                <img src={xl} alt="xl" className=" mr-4" />
-                <div>Pulsa 50.000</div>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
         <div className="mb-28"></div>
       </div>
