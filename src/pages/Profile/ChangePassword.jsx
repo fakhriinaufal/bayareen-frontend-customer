8ca7ca71cb008a31f9f7c02c13292426547e443e
Francisco <fcisco.dev@gmail.com>
2021-12-27 16:21:28 +0700

    add padding top

--- src/pages/Profile/ChangePassword.jsx
+++ src/pages/Profile/ChangePassword.jsx
@@ -1,18 +1,19 @@
-import HeaderSecond from "../../components/Header/HeaderSecond"
-import Navbar from "../../components/Navbar/Navbar"
-import Layout from "../../components/Layout/Layout"
-import Input from "../../components/Input/Input"
-import Button from "../../components/Button/Button"
+import HeaderSecond from "../../components/Header/HeaderSecond";
+import Navbar from "../../components/Navbar/Navbar";
+import Layout from "../../components/Layout/Layout";
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
 
 export default function ChangePassword() {
-    return (
-        <Layout head={<HeaderSecond />} nav={<Navbar />}>
-            <form className="mt-14" action="">
-                <Input text={"Password"} type={"password"} />
-                <Input text={"New Password"} type={"password"} />
-                <Input text={"Re-enter New Password"} type={"password"} />
-                <Button text={"Submit"} className={"mt-10"} />
-            </form>
-        </Layout>
-    )
+  return (
+    <Layout head={<HeaderSecond />} nav={<Navbar />}>
+      <div className="pt-14"></div>
+      <form action="">
+        <Input text={"Password"} type={"password"} />
+        <Input text={"New Password"} type={"password"} />
+        <Input text={"Re-enter New Password"} type={"password"} />
+        <Button text={"Submit"} className={"mt-10"} />
+      </form>
+    </Layout>
+  );
 }
