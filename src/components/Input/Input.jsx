b3ae9b48215f2171e6b636415690415a5d34726f
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-18 14:24:58 +0800

    add react-form-hooks

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import React from "react";
 
-export default function Input({ name, text, type, value, onChange, disabled = false, containerClassName, labelClassName, inputClassName }) {
-    return (
-        <div className={`flex flex-col gap-1 mt-5 ${containerClassName}`}>
-            <label htmlFor={name} className={`text-dark-green pl-1 ${labelClassName}`}>{text}</label>
-            <input 
-                id={name} 
-                type={type || 'text'} 
-                value={value} 
-                onChange={onChange} 
-                disabled={disabled}
-                className={`bg-light-gray text-dark-green p-2 rounded-md focus:outline-dark-green ${inputClassName}`}/>
-        </div>
-    )
+export default function Input({
+  name,
+  text,
+  type,
+  value,
+  onChange,
+  disabled = false,
+  register,
+  required,
+  requiredMsg,
+  containerClassName,
+  labelClassName,
+  inputClassName,
+}) {
+  return (
+    <div className={`flex flex-col gap-1 mt-5 ${containerClassName}`}>
+      <label
+        htmlFor={name}
+        className={`text-dark-green pl-1 ${labelClassName}`}
+      >
+        {text}
+      </label>
+
+      {register ? (
+        <input
+          id={name}
+          type={type || "text"}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          className={`bg-light-gray text-dark-green p-2 rounded-md focus:outline-dark-green ${inputClassName}`}
+          {...register(name, { required: requiredMsg })}
+        />
+      ) : (
+        <input
+          id={name}
+          type={type || "text"}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
+          className={`bg-light-gray text-dark-green p-2 rounded-md focus:outline-dark-green ${inputClassName}`}
+        />
+      )}
+    </div>
+  );
 }
