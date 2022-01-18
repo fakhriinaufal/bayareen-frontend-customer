34868a2d5be6c47dc610b06e068e1aa8d3a32a57
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-18 14:16:16 +0800

    create global state management

---
+++ src/store/userSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    data: {
+      id: 0,
+      name: "",
+      phone: "",
+      email: "",
+      token: "",
+      isLogin: false,
+    },
+  },
+  reducers: {
+    setData: (state, action) => {
+      state.data = action.payload;
+      state.data.isLogin = true;
+    },
+  },
+});
+
+export const { setData } = userSlice.actions;
+export default userSlice;
