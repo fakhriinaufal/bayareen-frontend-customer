02d21c5c95d6dd3c5dde9d12a3c718df7d51dc44
“Ivan <hegiansyahivan016@gmail.com>
2022-01-19 00:17:38 +0800

    add logic and validation form

--- src/pages/Profile/ChangePassword.jsx
+++ src/pages/Profile/ChangePassword.jsx
@@ -3,16 +3,68 @@ import Navbar from "../../components/Navbar/Navbar";
 import Layout from "../../components/Layout/Layout";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import { useState } from "react";
 
 export default function ChangePassword() {
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (oldPassword === "" || newPassword === "" || confirmPassword === "") {
+      setError("Required field must be filled");
+      return;
+    }
+    if (newPassword !== confirmPassword) {
+      setError("Password doesn't match");
+      return;
+    }
+  };
+
+  const onChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === "old-password") {
+      setOldPassword(value);
+    }
+    if (name === "new-password") {
+      setNewPassword(value);
+    }
+    if (name === "confirm-password") {
+      setConfirmPassword(value);
+    }
+  };
   return (
     <Layout head={<HeaderSecond />} nav={<Navbar />}>
       <div className="pt-14"></div>
-      <form action="">
-        <Input text={"Password"} type={"password"} />
-        <Input text={"New Password"} type={"password"} />
-        <Input text={"Re-enter New Password"} type={"password"} />
-        <Button text={"Submit"} className={"mt-10"} />
+      <form onSubmit={submitHandler}>
+        <Input
+          text={"Password"}
+          type={"password"}
+          name={"old-password"}
+          value={oldPassword}
+          onChange={onChange}
+        />
+        <Input
+          text={"New Password"}
+          type={"password"}
+          name={"new-password"}
+          value={newPassword}
+          onChange={onChange}
+        />
+        <Input
+          text={"Re-enter New Password"}
+          type={"password"}
+          name={"confirm-password"}
+          value={confirmPassword}
+          onChange={onChange}
+          containerClassName={"mb-4"}
+        />
+        {error && <p className="text-sm text-red-500">{error}</p>}
+        <Button text={"Submit"} className={"mt-5"} />
       </form>
     </Layout>
   );
--- src/pages/Profile/EditProfile.jsx
+++ src/pages/Profile/EditProfile.jsx
@@ -3,16 +3,72 @@ import HeaderSecond from "../../components/Header/HeaderSecond";
 import Navbar from "../../components/Navbar/Navbar";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function EditProfile() {
+  const data = useSelector((state) => state.user.data);
+
+  const [name, setName] = useState(data.name);
+  const [phone, setPhone] = useState(data.phone);
+  const [email, setEmail] = useState(data.email);
+  const [error, setError] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (name === data.name && phone === data.phone && email === data.email) {
+      setError("Nothing changes");
+      return;
+    }
+    if (name === "" || phone === 0 || email === "") {
+      setError("Required field must be filled");
+      return;
+    }
+  };
+
+  const changeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    if (name === "name") {
+      setName(value);
+    }
+    if (name === "phone") {
+      setPhone(value);
+    }
+    if (name === "email") {
+      setEmail(value);
+    }
+  };
+
   return (
     <Layout head={<HeaderSecond />} nav={<Navbar />}>
       <div className="pt-14"></div>
-      <form action="">
-        <Input text={"Name"} />
-        <Input text={"Phone Number"} type={"number"} />
-        <Input text={"Email"} />
-        <Button text={"Submit"} className={"mt-10"} />
+      <form onSubmit={submitHandler}>
+        <Input
+          name={"name"}
+          text={"Name"}
+          onChange={changeHandler}
+          value={name}
+        />
+        <Input
+          name={"phone"}
+          text={"Phone Number"}
+          type={"number"}
+          onChange={changeHandler}
+          value={phone}
+        />
+
+        <Input
+          name={"email"}
+          text={"Email"}
+          type={"email"}
+          onChange={changeHandler}
+          value={email}
+          containerClassName={"mb-4"}
+        />
+        {error && <p className="text-sm text-red-500">{error}</p>}
+        <Button text={"Submit"} className={"mt-5"} />
       </form>
     </Layout>
   );
