3d6bfe36a24a57aea6fbb1884f30701308fb9fe1
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-18 14:20:41 +0800

    create getCategories hooks

---
+++ src/hooks/useGetCategories.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function useGetCategories() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/categories")
+      .then((res) => {
+        const convertCategories = res.data.data.map((cat) => {
+          return {
+            text: cat.name,
+            val: cat.id,
+          };
+        });
+        setCategories(convertCategories);
+        setLoading(false);
+      })
+      .catch((err) => setError(err));
+  }, []);
+
+  return { categories, loading, error };
+}
