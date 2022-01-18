fd11dcc64cff3108e93ce82d1f14ba49dd2ca13e
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-18 14:39:55 +0800

    create public route and private route

---
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function PrivateRoute() {
+  const isLogin = useSelector((state) => state.user.data.isLogin);
+  console.log(isLogin, "islogin");
+  return isLogin ? <Outlet /> : <Navigate to="/not-login" />;
+}
+
+export default PrivateRoute;
