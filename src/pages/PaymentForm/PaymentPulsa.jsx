67fb04ff57954b28d2e5c59d903942877e0a7954
Ivan Hegiansyah <hegiansyahivan016@gmail.com>
2022-01-18 14:34:28 +0800

    add createTransactions hooks

--- src/pages/PaymentForm/PaymentPulsa.jsx
+++ src/pages/PaymentForm/PaymentPulsa.jsx
@@ -3,17 +3,54 @@ import Header from "../../components/Header/HeaderSecond";
 import Navbar from "../../components/Navbar/Navbar";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import { useLocation } from "react-router-dom";
+import useCreateTransaction from "../../hooks/useCreateTransaction";
+import { useSelector } from "react-redux";
+import ReactLoading from "react-loading";
 
 export default function PaymentPulsa() {
+  const { state } = useLocation();
+
+  const userId = useSelector((state) => state.user.data.id);
+  const { createTransaction, loading, error, url } = useCreateTransaction();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const object = {
+      user_id: userId,
+      product_id: state.nominal.val,
+    };
+    createTransaction(object);
+  };
+  if (url !== "") {
+    console.log("test");
+    window.location.replace(url);
+  }
+
   return (
     <Layout head={<Header />} nav={<Navbar />}>
       <h3 className="pt-16 text-dark-green text-2xl font-semibold">Payment</h3>
-      <form action="">
-        <Input value={"081297149324"} text={"Nomor"} disabled={true} />
-        <Input value={"Telkomsel"} text={"Provider"} disabled={true} />
-        <Input value={"Rp100.000"} text={"Nominal"} disabled={true} />
-        <Input value={"Rp102.500"} text={"Total Price"} disabled={true} />
-        <Button text={"Pay"} className={"mt-10"} />
+      <form onSubmit={submitHandler}>
+        <Input value={state.no} text={"Nomor"} disabled={true} />
+        <Input value={state.provider.text} text={"Provider"} disabled={true} />
+        <Input value={state.nominal.text} text={"Nominal"} disabled={true} />
+        <Input
+          value={state.nominal.price}
+          text={"Total Price"}
+          disabled={true}
+        />
+        {!loading ? (
+          <Button text={"Pay"} className={"mt-10"} />
+        ) : (
+          <ReactLoading
+            type={"spokes"}
+            color={"#83C5BE"}
+            height={50}
+            width={50}
+            className="mx-auto mt-10"
+          />
+        )}
+        {error && <p>{error.message}</p>}
       </form>
     </Layout>
   );
