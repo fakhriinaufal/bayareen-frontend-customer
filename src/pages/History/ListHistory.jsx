ef1890b082cb61e63fc8bfe83fb3182cbf9574c0
“Ivan <hegiansyahivan016@gmail.com>
2022-01-21 01:18:06 +0800

    change color status

--- src/pages/History/ListHistory.jsx
+++ src/pages/History/ListHistory.jsx
@@ -21,7 +21,7 @@ export default function ListHistory(props) {
   return (
     <Layout head={<Header2 />} nav={<Navbar />}>
       <div className="flex flex-col">
-        <div className="mt-16 text-dark-green font-bold font-main">
+        <div className="pt-16 text-dark-green text-2xl font-semibold">
           Last Purchased
         </div>
         {loading ? (
